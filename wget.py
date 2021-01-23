0aba5746ef5920492890e9c473b14d8e6d4c181b
Lorenzo <56091505+lgardea@users.noreply.github.com>
2021-01-23 12:34:26 -0800

    simplified the function, added stderr support

--- wget.py
+++ wget.py
@@ -8,38 +8,16 @@ def wget(host):
     Returns:
         int: 1 if website is down, 0 if website is up
     """
-    # create socket
-    print('# Creating socket')
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     except socket.error:
-        print('Error: Socket')
+        sys.stderr.write('Error: Socket\n')
         sys.exit()
 
-    print('# Getting remote IP address') 
     try:
         remote_ip = socket.gethostbyname( host )
     except socket.gaierror:
-        print('Website is down!')
+        sys.stderr.write('Website ' + host + ' is down!\n')
         return 1
 
-    # Connect to remote server
-    print('# Connecting to server, ' + host + ' (' + remote_ip + ')')
-    s.connect((remote_ip , 80))
-
-    # Send data to remote server
-    print('# Sending data to server')
-    request = "GET / HTTP/1.0\r\n\r\n"
-
-    try:
-        s.sendall(request)
-    except socket.error:
-        print 'Error: Socket'
-        sys.exit()
-
-    # Receive data
-    print('# Receive data from server')
-    reply = s.recv(4096)
-
-    print reply
     return 0
