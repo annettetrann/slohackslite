6e7c881a6ccb19131422691de9dfc6320f14ffa5
Lorenzo <56091505+lgardea@users.noreply.github.com>
2021-01-23 16:19:36 -0800

    timer/main example

--- timer.py
+++ timer.py
@@ -8,18 +8,24 @@ def cycleSites(websites):
         Returns:
             None
     ''' 
-    timer = threading.Timer(3.0, cycleSites, args)
+    timer = threading.Timer(4.0, cycleSites, args)
     timer.daemon = True 
     timer.start()
     for site in websites:
         wget(site)
 
-websites = ["www.google.com", "www.thiswebsiteisdown.com"]
+websites = []
 args = [websites]
 cycleSites([])
 while True:
     host = raw_input("Enter URL\n")
     if host == "exit":
         break
+    if host == "remove":
+        re = raw_input("Enter site to remove\n")
+        if websites.count(re) == 1:
+            websites.remove(re)
+            print("Removed " + re)
     else:
-        websites.append(host)
+        if websites.count(host) == 0:
+            websites.append(host)
